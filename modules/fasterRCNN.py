c498c977bdb3d044eb57e3e82c463d738252d375
CharlesPikachu <1159254961@qq.com>
2020-03-08 23:58:34 +0800

    fix dim errror

--- modules/fasterRCNN.py
+++ modules/fasterRCNN.py
@@ -230,9 +230,9 @@ class fasterRCNNFPNBase(nn.Module):
 		else:
 			raise ValueError('Unkown pooling_method <%s> in fasterRCNNFPNBase...' % self.pooling_method)
 		# feed into top model
-		pooled_features = self.top_model(pooled_features)
 		if len(pooled_features.size()) == 4:
 			pooled_features = pooled_features.mean(3).mean(2)
+		pooled_features = self.top_model(pooled_features)
 		# predict location
 		x_loc = self.fc_loc(pooled_features)
 		if self.mode == 'TRAIN' and not self.is_class_agnostic:
